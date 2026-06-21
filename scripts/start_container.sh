95e8e183e17fa4cd01cb20eab1328fa783691988
JaganNampally <33394259+JaganNampally@users.noreply.github.com>
2026-06-21 23:54:26 +0530

    Update start_container.sh 6

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,6 @@ set -e
     
 # Pull the Docker image from Docker Hub
 docker pull jagandock/simple-python-app
- 
+  
 # Run the Docker image as a container
 docker run -d -p 5000:5000 jagandock/simple-python-app
