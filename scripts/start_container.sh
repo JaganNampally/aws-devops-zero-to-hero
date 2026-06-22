56a41d6a6b013002a2a11ed4b48b452a31faad08
JaganNampally <33394259+JaganNampally@users.noreply.github.com>
2026-06-23 00:25:56 +0530

    Update start_container.sh tag added

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-    
+IMAGE_TAG=$(cat image_tag.txt)
 # Pull the Docker  image from Docker Hub
-docker pull jagandock/simple-python-app
+docker pull jagandock/simple-python-app:$IMAGE_TAG
    
 # Run the Docker image as a container 
-docker run -d -p 5000:5000 jagandock/simple-python-app
+docker run -d -p 5000:5000 jagandock/simple-python-app:$IMAGE_TAG
