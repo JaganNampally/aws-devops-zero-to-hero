50a6991e23171aff99bd5517568bd4057a1df67b
JaganNampally <33394259+JaganNampally@users.noreply.github.com>
2026-06-23 22:44:42 +0530

    Update start_container.sh f

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
+
+echo "Current directory:"
+pwd
+
+echo "Files in current directory:"
+ls -la
+
+echo "Searching for image_tag.txt:"
+find /opt/codedeploy-agent -name image_tag.txt 2>/dev/null
+
 IMAGE_TAG=$(cat image_tag.txt)
 # Pull the Docker  image from Docker Hub
 docker pull jagandock/simple-python-app:$IMAGE_TAG
